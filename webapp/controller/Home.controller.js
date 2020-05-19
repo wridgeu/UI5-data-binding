24e6a13b822b1d2c3a836fcb8ce4e181752da2bd
Marco Beier <marco-beier@t-online.de>
2020-05-19 22:03:32 +0200

    Added everything for Step 2: Creating a Model

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,18 @@
 sap.ui.define([
-  "com/mrb/UI5-Data-Binding/controller/BaseController"
-], function(Controller) {
+  "com/mrb/UI5-Data-Binding/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
   "use strict";
 
-  return Controller.extend("com.mrb.UI5-Data-Binding.controller.Home", {});
+  return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
+    onInit: function(){
+      
+      var oModel = new JSONModel({
+        greetingText: "Hi, my name is Marco"
+      });
+
+      //Set model globally for all Views/Controls (Dirty)
+      sap.ui.getCore().setModel(oModel);
+    }
+  });
 });
