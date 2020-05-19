520bc97ca38b3a7595d311459f03a4a202fda24c
Marco Beier <marco-beier@t-online.de>
2020-05-19 21:44:07 +0200

    rooting config

---
+++ webapp/controller/App.Controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"com/mrb/UI5-Data-Binding/controller/BaseController"
+], function(
+	BaseController
+) {
+	"use strict";
+
+	return BaseController.extend("com.mrb.UI5-Data-Binding.controller.App.Controller", {
+	});
+});
