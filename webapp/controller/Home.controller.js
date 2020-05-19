79ea2937a7b2d40937c1358f7c7a77b5a47a2103
Marco Beier <marco-beier@t-online.de>
2020-05-19 22:25:53 +0200

    Added everything for Step 3: Create Property Binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,18 +1,22 @@
-sap.ui.define([
-  "com/mrb/UI5-Data-Binding/controller/BaseController",
-  "sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
-  "use strict";
+sap.ui.define(
+  [
+    "com/mrb/UI5-Data-Binding/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (BaseController, JSONModel) {
+    "use strict";
 
-  return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
-    onInit: function(){
-      
-      var oModel = new JSONModel({
-        greetingText: "Hi, my name is Marco"
-      });
+    return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
+      onInit: function () {
+        var oModel = new JSONModel({
+          greetingText: "Hi, my name is Marco",
+        });
 
-      //Set model globally for all Views/Controls (Dirty)
-      sap.ui.getCore().setModel(oModel);
-    }
-  });
-});
+        //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""        
+        //the go-to way for global definition would be to define the model within the manifest.json-file
+        //and retrieve it via "this.getOwnerComponent().getModel();"
+        this.getView().setModel(oModel);
+      },
+    });
+  }
+);
