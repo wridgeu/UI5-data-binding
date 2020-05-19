0937975ca1f78eb659981d6632dad19d701e78cd
Marco Beier <marco-beier@t-online.de>
2020-05-19 21:22:47 +0200

    Initialize repository with easy-ui5

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/mrb/UI5-Data-Binding/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.mrb.UI5-Data-Binding.controller.Home", {});
+});
