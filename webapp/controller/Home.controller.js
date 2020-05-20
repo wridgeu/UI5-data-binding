ff5114c100ac587ef9bae4cde9ca8ede74d25fa0
Marco Beier <marco-beier@t-online.de>
2020-05-20 22:28:15 +0200

    Added everything for Step 13: Element Binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(
   [
     "com/mrb/UI5-Data-Binding/controller/BaseController",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
   ],
   function (BaseController, JSONModel) {
     "use strict";
@@ -12,7 +12,7 @@ sap.ui.define(
         var oModel, oMessageManager, oProductModel;
 
         oProductModel = new JSONModel();
-		    oProductModel.loadData("./model/Products.json");
+        oProductModel.loadData("./model/Products.json");
 
         oModel = new JSONModel({
           firstName: "Harry",
@@ -22,10 +22,10 @@ sap.ui.define(
             street: "Dietmar-Hopp-Allee 16",
             city: "Walldorf",
             zip: "69190",
-            country: "Germany"
+            country: "Germany",
           },
           salesToDate: 12345.6789,
-          currencyCode: "EUR"
+          currencyCode: "EUR",
         });
 
         //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""
@@ -34,11 +34,18 @@ sap.ui.define(
         this.getView().setModel(oModel);
 
         //set loaded JSON file as model with name 'products'
-		    this.getView().setModel(oProductModel, "products");
-        
+        this.getView().setModel(oProductModel, "products");
+
         //MessageManager is a singleton
         oMessageManager = sap.ui.getCore().getMessageManager();
         oMessageManager.registerObject(this.getView(), true);
+      },
+      onItemSelected: function (oEvent) {
+        var oSelectedItem = oEvent.getSource();
+        var oContext = oSelectedItem.getBindingContext("products");
+        var sPath = oContext.getPath();
+        var oProductDetailPanel = this.byId("productDetailsPanel");
+        oProductDetailPanel.bindElement({ path: sPath, model: "products" });
       },
     });
   }
