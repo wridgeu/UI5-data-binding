4514324a58abf1622e5833329e886814f1d2b78e
Marco Beier <marco-beier@t-online.de>
2020-05-20 19:12:11 +0200

    Added everything for Step 4: Two-Way Data Binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -8,11 +8,15 @@ sap.ui.define(
 
     return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
       onInit: function () {
+        // Create a JSON model from an object literal
         var oModel = new JSONModel({
-          greetingText: "Hi, my name is Marco",
+          firstName: "Harry",
+          lastName: "Hawk",
+          enabled: true,
+          panelHeaderText: "Data Binding Basics",
         });
 
-        //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""        
+        //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""
         //the go-to way for global definition would be to define the model within the manifest.json-file
         //and retrieve it via "this.getOwnerComponent().getModel();"
         this.getView().setModel(oModel);
