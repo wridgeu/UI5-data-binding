37c99705dd6388f53f39a43b3fd6c621864ea423
Marco Beier <marco-beier@t-online.de>
2023-02-12 15:41:31 +0100

    chore: bump deps, move to UI5 CLI 3.0, lint & formatting, add Core as dep

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,78 +1,79 @@
 sap.ui.define([
-    "com/mrb/UI5-Data-Binding/controller/BaseController",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/ObjectAttribute"
-  ], function (BaseController, JSONModel, ObjectAttribute) {
-    "use strict";
+  "com/mrb/UI5-Data-Binding/controller/BaseController",
+  "sap/ui/model/json/JSONModel",
+  "sap/m/ObjectAttribute",
+  "sap/ui/core/Core"
+], function (BaseController, JSONModel, ObjectAttribute, Core) {
+  "use strict";
 
-    return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
-      onInit: function () {
-        // Create a JSON model from an object literal
-        var oModel, oMessageManager, oProductModel;
+  return BaseController.extend("com.mrb.UI5-Data-Binding.controller.Home", {
+    onInit: function () {
+      // Create a JSON model from an object literal
+      var oModel, oMessageManager, oProductModel;
 
-        oProductModel = new JSONModel();
-        oProductModel.loadData("./model/Products.json");
+      oProductModel = new JSONModel();
+      oProductModel.loadData("./model/Products.json");
 
-        oModel = new JSONModel({
-          firstName: "Harry",
-          lastName: "Hawk",
-          enabled: true,
-          address: {
-            street: "Dietmar-Hopp-Allee 16",
-            city: "Walldorf",
-            zip: "69190",
-            country: "Germany",
-          },
-          salesToDate: 12345.6789,
-          priceThreshold: 20,
-          currencyCode: "EUR",
-        });
+      oModel = new JSONModel({
+        firstName: "Harry",
+        lastName: "Hawk",
+        enabled: true,
+        address: {
+          street: "Dietmar-Hopp-Allee 16",
+          city: "Walldorf",
+          zip: "69190",
+          country: "Germany",
+        },
+        salesToDate: 12345.6789,
+        priceThreshold: 20,
+        currencyCode: "EUR",
+      });
 
-        //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""
-        //the go-to way for global definition would be to define the model within the manifest.json-file
-        //and retrieve it via "this.getOwnerComponent().getModel();"
-        this.getView().setModel(oModel);
+      //Set model globally for all views/controls (dirty) "sap.ui.getCore().setModel(oModel);""
+      //the go-to way for global definition would be to define the model within the manifest.json-file
+      //and retrieve it via "this.getOwnerComponent().getModel();"
+      this.getView().setModel(oModel);
 
-        //set loaded JSON file as model with name 'products'
-        this.getView().setModel(oProductModel, "products");
+      //set loaded JSON file as model with name 'products'
+      this.getView().setModel(oProductModel, "products");
 
-        //MessageManager is a singleton
-        oMessageManager = sap.ui.getCore().getMessageManager();
-        oMessageManager.registerObject(this.getView(), true);
-      },
-      onItemSelected: function (oEvent) {
-        var oSelectedItem = oEvent.getSource();
-        var oContext = oSelectedItem.getBindingContext("products");
-        var sPath = oContext.getPath();
-        var oProductDetailPanel = this.byId("productDetailsPanel");
-        oProductDetailPanel.bindElement({ path: sPath, model: "products" });
-      },
-      productListFactory: function (sId, oContext) {
-        var oUIControl;
+      //MessageManager is a singleton
+      oMessageManager = Core.getMessageManager();
+      oMessageManager.registerObject(this.getView(), true);
+    },
+    onItemSelected: function (oEvent) {
+      var oSelectedItem = oEvent.getSource();
+      var oContext = oSelectedItem.getBindingContext("products");
+      var sPath = oContext.getPath();
+      var oProductDetailPanel = this.byId("productDetailsPanel");
+      oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+    },
+    productListFactory: function (sId, oContext) {
+      var oUIControl;
 
-        // Decide based on the data which dependent to clone
-        if (oContext.getProperty("UnitsInStock") === 0
-              && oContext.getProperty("Discontinued")) {
-          // The item is discontinued, so use a StandardListItem
-          oUIControl = this.byId("productSimple").clone(sId);
-        } else {
-          // The item is available, so we will create an ObjectListItem
-          oUIControl = this.byId("productExtended").clone(sId);
+      // Decide based on the data which dependent to clone
+      if (oContext.getProperty("UnitsInStock") === 0
+        && oContext.getProperty("Discontinued")) {
+        // The item is discontinued, so use a StandardListItem
+        oUIControl = this.byId("productSimple").clone(sId);
+      } else {
+        // The item is available, so we will create an ObjectListItem
+        oUIControl = this.byId("productExtended").clone(sId);
 
-          // The item is temporarily out of stock, so we will add a status
-          if (oContext.getProperty("UnitsInStock") < 1) {
-            oUIControl.addAttribute(
-              new ObjectAttribute({
-                text: {
-                  path: "i18n>outOfStock",
-                },
-              })
-            );
-          }
+        // The item is temporarily out of stock, so we will add a status
+        if (oContext.getProperty("UnitsInStock") < 1) {
+          oUIControl.addAttribute(
+            new ObjectAttribute({
+              text: {
+                path: "i18n>outOfStock",
+              },
+            })
+          );
         }
+      }
 
-        return oUIControl;
-      },
-    });
-  }
+      return oUIControl;
+    },
+  });
+}
 );
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,19 +2,20 @@ sap.ui.define([
 	"sap/m/library",
 	"sap/ui/core/Locale",
 	"sap/ui/core/LocaleData",
-	"sap/ui/model/type/Currency"
-], function (mobileLibrary, Locale, LocaleData, Currency) {
+	"sap/ui/model/type/Currency",
+	"sap/ui/core/Core"
+], function (mobileLibrary, Locale, LocaleData, Currency, Core) {
 	"use strict";
 	return {
-		formatMail: function(sFirstName, sLastName) {
+		formatMail: function (sFirstName, sLastName) {
 			var oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			return mobileLibrary.URLHelper.normalizeEmail(
 				sFirstName + "." + sLastName + "@example.com",
 				oBundle.getText("mailSubject", [sFirstName]),
 				oBundle.getText("mailBody"));
 		},
-		formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
-			var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+		formatStockValue: function (fUnitPrice, iStockLevel, sCurrCode) {
+			var sBrowserLocale = Core.getConfiguration().getLanguage();
 			var oLocale = new Locale(sBrowserLocale);
 			var oLocaleData = new LocaleData(oLocale);
 			var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
